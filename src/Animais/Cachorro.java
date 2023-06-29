8122db237eb1b18405132870077425d07e8699a9
Eduardo <eduardocasagrande71@gmail.com>
2023-06-29 00:52:09 -0300

    02. Classes e métodos

---
+++ src/Animais/Cachorro.java
@@ -0,0 +1,39 @@
+package Animais;
+
+public class Cachorro {
+
+  // Como a class foi criada em um package, as variáveis
+  // do cachorro só podem ser acessadas com o public
+  public String nome;
+  public String cor;
+  public int altura;
+  public double peso;
+  public int tamanhoDoRabo = 5;
+
+  public String estadoDeEspirito;
+
+  // Methods
+  // Retorna um void
+  public void latir() {
+    System.out.println("Au au!");
+  }
+
+  // retorna uma String
+  public String pegar() {
+
+    return "Peguei!";
+  }
+
+  public String interagir(String acao) {
+    if (acao.equals("carinho")) {
+      this.estadoDeEspirito = "Feliz";
+    } else if (acao.equals("vai dormir!")) {
+      this.estadoDeEspirito = "Bravo";
+    } else {
+      this.estadoDeEspirito = "Neutro";
+    }
+
+    return estadoDeEspirito;
+  }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@
 // O JVM (Java Virtual Machine) é quem chama o método main()
 // ele entende que o método main SEMPRE será o primeiro a ser compilado
 
+import Animais.Cachorro;
+
 public class Main {
 
     // static public void main(String... args)
@@ -16,6 +18,28 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+
+        // Classe - Variável -- Instância
+        Cachorro cachorro1 = new Cachorro();
+
+        cachorro1.nome = "Rex";
+        cachorro1.cor = "Branco";
+        cachorro1.altura = 30;
+        cachorro1.peso = 10.3;
+        // cachorro1.tamanhoDoRabo = 10;
+
+        System.out.println(cachorro1); // Animais.Cachorro@76ed6628
+        // package.class @ endereço na memoria
+        // Animais.Cachorro@76ed6628
+
+        cachorro1.latir();
+
+        // cachorro1.pegar(); // não printa nada, pois o método voltar um return
+        System.out.println(cachorro1.pegar()); // Peguei!
+
+        System.out.println("O cachorro esta " + cachorro1.interagir("carinho")); // O cachorro esta Feliz
+        System.out.println("O cachorro esta " + cachorro1.interagir("nada")); // O cachorro esta Neutro
+        System.out.println("O cachorro esta " + cachorro1.interagir("vai dormir!")); // O cachorro esta Bravo
 
     }
 }
